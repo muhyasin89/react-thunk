96082747741ab0f0422c848b9a2580e7463d83b6
muhyasin89 <muhyasin89@gmail.com>
2022-05-01 06:42:21 +0700

    [main]reinstall react

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header(){
+    return (
+        <header className="App-header">
+            <p className="lg:hidden">Test</p>
+            <p>Show</p>
+        </header>
+    );
+}
+
+export default Header;
